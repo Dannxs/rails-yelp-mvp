af6c04f98d3b72959a977487d9cfcf21a196f113
Danny Cardoso <dcardoso066@gmail.com>
2021-05-12 14:25:36 +0200

    exercice 2 completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,38 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.new(
+  name: 'Restaurant Kootchi',
+  address: '40 Rue du Cardinal Lemoine',
+  phone_number:  '01 44 07 20 56',
+  category: 'belgian'
+).save
+
+Restaurant.new(
+  name: 'Happypasta.paris',
+  address: '15 Rue Washington',
+  phone_number: '06 17 09 07 70',
+  category: "italian"
+).save
+
+Restaurant.new(
+  name: 'Sur Un Arbre Perché',
+  address: '1 Rue du 4 septembre',
+  phone_number: '01 42 96 97 01',
+  category: 'french'
+).save
+
+Restaurant.new(
+  name: 'Le Grand Colbert',
+  address: '2 Rue Vivienne',
+  phone_number: '01 42 86 87 88',
+  category: 'chinese'
+).save
+
+Restaurant.new(
+  name: 'KOUCHI YAKI',
+  address: '122 Rue Oberkampf',
+  phone_number: '01 43 55 46 61',
+  category: "japanese"
+).save
